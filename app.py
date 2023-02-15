ebdf2d2bfdb453acb60e64bfe51bcfb1ca81dfc8
janvandinther <jan@vandinther.nl>
2023-02-15 11:16:32 +0000

    html

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from viktor import ViktorController
 from viktor.parametrization import ViktorParametrization
 
+from pathlib import Path
+
+from viktor.views import WebResult
+from viktor.views import WebView
+
+
 
 class Parametrization(ViktorParametrization):
     pass
 
 
 class Controller(ViktorController):
-    viktor_enforce_field_constraints = True  # Resolves upgrade instruction https://docs.viktor.ai/sdk/upgrades#U83
+    viktor_enforce_field_constraints = True  # prevents a warning and can be ignored for the purpose of this guide
 
-    label = 'My Entity Type'
-    parametrization = Parametrization
+    label = "ABM Kampala"              # label to be shown in the interface
+    @WebView('ABM Kampala', duration_guess=1)
+    def get_web_view(self, params, **kwargs):
+        static_html_path = Path(__file__).parent / 'boda-boda-charging.html'
+        return WebResult.from_path(static_html_path)
